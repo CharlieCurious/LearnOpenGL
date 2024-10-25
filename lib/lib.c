007e56fde74cf94fa1f9f19365fd3185b0881322
CharlieCurious <dfrco2@outlook.com>
2024-10-25 16:40:04 +0300

    Added lib with common functions and test sector.

---
+++ lib/lib.c
@@ -0,0 +1,37 @@
+#include <lib.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *load_shaders(FILE *shaders_file) {
+    if(fseek(shaders_file, 0L, SEEK_END) != 0) {
+        perror("Failed to determine shader source size.");
+        return NULL;
+    }
+    
+    long length = ftell(shaders_file);
+    if (length < 0 || length > MAX_FILE_SIZE) {
+        perror("File size error of file too large.");
+        return NULL;
+    }
+
+    rewind(shaders_file);
+
+    char *content = (char *)malloc(length + 2); // +2 for /n and /0.
+    if (!content) {
+        perror("Memory allocation failed.");
+        return NULL;
+    }
+
+    size_t read_size = fread(content, 1, length, shaders_file);
+
+    // Handle incomplete reads
+    if (read_size != length) {
+        perror("Failed to read the entire file.");
+        free(content);
+        return NULL;
+    }
+
+    memcpy(content + length, "\n\0", 2);
+
+    return content;
+}
---
+++ lib/lib.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <stdio.h>
+
+#define MAX_FILE_SIZE 1048576 // 1 MB
+
+char *load_shaders(FILE *shaders_file);
