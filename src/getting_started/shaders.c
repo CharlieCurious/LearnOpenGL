a1ca4da90c63604749cb4f40135075efd87816a5
CharlieCurious <dfrco2@outlook.com>
2024-10-26 20:05:45 +0300

    Triangle changing color.

--- src/getting_started/shaders.c
+++ src/getting_started/shaders.c
@@ -1,12 +1,31 @@
 #include <lib.h>
+#include <math.h>
+#include <stdbool.h>
 #include <stdlib.h>
 
 void framebufferSizeCallback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-int main(int argc, const char **argv) {
+const char *vertexShaderSource ="#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos, 1.0);\n"
+    "}\n\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "uniform vec4 ourColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = ourColor;\n"
+    "}\n\0";
+
+
+int main() {
 
     // ------ Setup GLFW and glad ---------
     initializeGLFW();
@@ -17,9 +36,65 @@ int main(int argc, const char **argv) {
 
     loadOpenGLFunctionPointersOrExit();
 
+    // ------ Setup Shaders -------------
+    unsigned int shaderProgram = createShaderProgramOrExit(vertexShaderSource, fragmentShaderSource);
+
+    // ------ Setup vertex data and buffers and configure vertex attributes ------
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f, // bottom right
+        0.5f, -0.5f, 0.0f, // bottom left
+        0.0f, 0.5f, 0.0f    // top
+    };
+
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+
+    glBindVertexArray(VAO);
+
+    while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+
+        // render
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+
+        // update shader uniform
+        double timeValue = glfwGetTime();
+        float greenValue = (float)(sinf(timeValue) / 2.0 + 0.5);
+        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
+        // render the triangle
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
+
+    glfwTerminate();
     return EXIT_SUCCESS;
 }
 
 void framebufferSizeCallback(GLFWwindow *window, int width, int height) {
     glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
 }
