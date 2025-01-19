e15c795c12372c352b918891016a11705cc35555
CharlieCurious <dfrco2@outlook.com>
2025-01-19 02:06:04 +0200

    Light casters

--- learn_opengl/src/main.cpp
+++ learn_opengl/src/main.cpp
@@ -182,6 +182,19 @@ int main() {
         -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
     };
 
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f),
+        glm::vec3( 2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3( 2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3( 1.3f, -2.0f, -2.5f),
+        glm::vec3( 1.5f,  2.0f, -2.5f),
+        glm::vec3( 1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
+
     glEnable(GL_DEPTH_TEST);
 
     unsigned int VBO, cubeVAO, lightCubeVAO;
@@ -214,7 +227,7 @@ int main() {
 
     // --------------------------------------------------------------------------------------------------------//
 
-    Shader lightingShader("shaders/lighting/4.2.lighting_maps.vert", "shaders/lighting/4.2.lighting_maps.frag");
+    Shader lightingShader("shaders/lighting/5.light_casters.vert", "shaders/lighting/5.light_casters.frag");
     Shader lightCubeShader("shaders/1.light_cube.vert", "shaders/1.light_cube.frag");
 
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -243,6 +256,9 @@ int main() {
         lightingShader.setVec3("light.ambient",  0.2f, 0.2f, 0.2f);
         lightingShader.setVec3("light.diffuse",  0.5f, 0.5f, 0.5f); // darken diffuse light a bit
         lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+        lightingShader.setFloat("light.constant", 1.0f);
+        lightingShader.setFloat("light.linear", 0.09f);
+        lightingShader.setFloat("light.quadratic", 0.032f);
 
         lightingShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
         lightingShader.setFloat("material.shininess", 32.0f);
@@ -254,7 +270,6 @@ int main() {
         lightingShader.setMat4("view", view);
 
         glm::mat4 model = glm::mat4(1.0f);
-        lightingShader.setMat4("model", model);
 
         // bind diffuse map
         glActiveTexture(GL_TEXTURE0);
@@ -263,7 +278,16 @@ int main() {
         glBindTexture(GL_TEXTURE_2D, specularMap);
 
         glBindVertexArray(cubeVAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        for (uint i = 0; i < 10; i++) {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            lightingShader.setMat4("model", model);
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
 
         lightCubeShader.use();
         lightCubeShader.setMat4("projection", projection);
